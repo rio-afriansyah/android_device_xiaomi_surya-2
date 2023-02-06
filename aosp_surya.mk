ecfe2277212ad25562e593a506b912772d138a48
Ryofry <68908824+rio-afriansyah@users.noreply.github.com>
2023-02-06 07:28:12 -0500

    Update and rename lineage_surya.mk to aosp_surya.mk

--- aosp_surya.mk
+++ aosp_surya.mk
@@ -12,9 +12,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_surya
+
+# Project-Elixir Official Stuff
+
+TARGET_BOOT_ANIMATION_RES := 1080
+
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+
+TARGET_INCLUDE_STOCK_ACORE := false
+
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+ELIXIR_MAINTAINER := Smokey
+
+
+PRODUCT_NAME := aosp_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
